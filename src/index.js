160d79eda44623e694bb2f10287a158ed8294b94
Eugene Allenov <imr@inbox.ru>
2018-10-23 18:36:19 +0300

    added task 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 const { getRandomWordSync, getRandomWord } = require('word-maker');
 
+console.log('It works!');
+
 // YOUR CODE HERE
+
+let i = 1;
+for (i; i <= 100; ++i) {
+  console.log(`${i}: ${getRandomWordSync()}`)
+}
