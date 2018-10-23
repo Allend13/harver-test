b12411dc01a3624e874f22e36bd15777bea9ae25
Eugene Allenov <imr@inbox.ru>
2018-10-23 18:55:59 +0300

    added task 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-const { getRandomWordSync, getRandomWord } = require('word-maker');
+const { getRandomWordSync, getRandomWord } = require('word-maker')
 
-console.log('It works!');
+
+const fizzBuzzer = (n) => {
+  const modulo3 = !(n % 3)
+  const modulo5 = !(n % 5)
+
+  if (modulo3 && modulo5) {
+    return "FizzBuzz"
+  } else if (modulo3) {
+    return 'Fizz'
+  } else if (modulo5) {
+    return 'Buzz'
+  } else {
+    return getRandomWordSync()
+  }
+}
 
 // YOUR CODE HERE
+let i = 1
 
-let i = 1;
 for (i; i <= 100; ++i) {
-  console.log(`${i}: ${getRandomWordSync()}`)
+  console.log(`${i}: ${fizzBuzzer(i)}`)
 }
